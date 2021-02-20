3be40f059ef35af49a89ca5a2b157619f583c32c
ashleighmm <ashleigh@hoistdigital.co.uk>
2021-02-20 20:17:39 +0000

    Added social component

---
+++ src/components/Social.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+                <a href="https://codepen.io/naafi" target="_blank"><i class="fab fa-codepen"></i></a>
+                <a href="https://github.com/ashleighmm" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://Instagram.com/ashmichmore" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/ashleigh-more-hattia-61b05693/" target="_blank"><i class="fab fa-linkedin-in"></i></a>  
+            </div>
+        )
+    }
+}
+export default Social;
