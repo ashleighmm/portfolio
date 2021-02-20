114eb0ad316715e0c846cfc8773a0a2518efd9c7
ashleighmm <ashleigh@hoistdigital.co.uk>
2021-02-20 15:20:26 +0000

    Navbar iems functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,51 @@
-import React , { Component } from 'react';
-import Navitem from './Navitem';
+import React, { Component } from "react";
+import Navitem from "./Navitem";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <nav>
-                <ul>
-                <Navitem item="Home" tolink="/" ></Navitem>
-                <Navitem item="About" tolink="/about" ></Navitem>
-                <Navitem item="Education" tolink="/education"></Navitem>
-                <Navitem item="Skills" tolink="/skills"></Navitem>
-                <Navitem item="Contact" tolink="/contact"></Navitem>
-                </ul>
-            </nav>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      NavitemActive: "",
+    };
+  }
+  activeItem = (x) => {
+    if (this.state.NavitemId.length > 0) {
+      document
+        .getElementById(this.state.NavitemActive)
+        .classList.remove("active");
     }
+    this.setState({ NavitemActive: x }, () => {
+      document.getElementById(this.state.NavitemActive).classList.add("active");
+    });
+  };
+  render() {
+    return (
+      <nav>
+        <ul>
+          <Navitem item="Home" tolink="/" activec={this.activeItem}></Navitem>
+          <Navitem
+            item="About"
+            tolink="/about"
+            activec={this.activeItem}
+          ></Navitem>
+          <Navitem
+            item="Education"
+            tolink="/education"
+            activec={this.activeItem}
+          ></Navitem>
+          <Navitem
+            item="Skills"
+            tolink="/skills"
+            activec={this.activeItem}
+          ></Navitem>
+          <Navitem
+            item="Contact"
+            tolink="/contact"
+            activec={this.activeItem}
+          ></Navitem>
+        </ul>
+      </nav>
+    );
+  }
 }
 export default Navbar;
